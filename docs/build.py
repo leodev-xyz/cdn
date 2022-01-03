833b739517892a63f195893d7771b950c501476f
Leo Developer <leodeveloper@protonmail.com>
2022-01-03 01:45:51 +0100

    add snippet support

--- docs/build.py
+++ docs/build.py
@@ -15,7 +15,10 @@ out.mkdir(exist_ok=True, parents=True)
 input = Path("docs/docs")
 for file in input.iterdir():
     if file.suffix == ".lua" and not file.stem.startswith("."):
-        doc = make_doc(f"docs/docs/{file.name}", True)
+        doc = make_doc(f"docs/docs/{file.name}")
         o = out / file.stem
         o.mkdir(exist_ok=True)
         (o / "docs.json").write_text(doc.json)
+        (o / "snippets").mkdir(exist_ok=True)
+        for snippet in doc.snippets:
+            (o / "snippets" / snippet).write_text(doc.snippets[snippet])
